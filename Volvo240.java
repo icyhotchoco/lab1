e3a2b710a0a578c750be6616ae2037198bb759e8
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-01-24 15:56:12 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -1,12 +1,13 @@
 import java.awt.*;
 
-public class Volvo240 extends Car{
-
-    public Volvo240(){
-        nrDoors = 4;
-        color = Color.black;
-        enginePower = 100;
-        modelName = "Volvo240";
+public class Volvo240 extends Car {
+    private final static double trimFactor = 1.25;
+    public Volvo240() {
+        super(4, 100,0, Color.black,"Volvo240");
         stopEngine();
+    }
+    @Override
+    public double speedFactor() {
+        return enginePower * 0.01 * trimFactor;
     }
 }
