3151d2417338ce1c6ef7f70369fcfe607810bebb
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-01-24 15:55:54 +0100

    Update Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -1,14 +1,22 @@
 import java.awt.*;
 
 public class Saab95 extends Car{
-
-    public Saab95(){
-        nrDoors = 2;
-        color = Color.red;
-        enginePower = 125;
-        turboOn = false;
-        modelName = "Saab95";
+    private boolean turboOn;
+    public Saab95() {
+        super(2, 125, 0, Color.red, "Saab95");
         stopEngine();
     }
+    public void setTurboOn() {
+        turboOn = true;
+    }
 
+    public void setTurboOff() {
+        turboOn = false;
+    }
+    @Override
+    public double speedFactor() {
+        double turbo = 1;
+        if(turboOn) turbo = 1.3;
+        return enginePower * 0.01 * turbo;
+    }
 }
