afd85df7f56e44b24e4552c187305bee6022046e
plopp0 <halimo.hassan.ali@outlook.com>
2026-01-28 15:56:11 +0100

    Create CarTest.java

---
+++ CarTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    private Car car = new Saab95();
+
+    @Test
+    void turnLeft() {
+        car.turnLeft();
+        assertEquals(270, car.getDirectionDeg());
+    }
+
+    @Test
+    void turnRight() {
+        car.turnRight();
+        assertEquals(90, car.getDirectionDeg());
+    }
+
+    @Test
+    void move() {
+        car.move();
+        assertEquals(car.getCurrentSpeed(), car.getY());
+        car.turnRight();
+        car.move();
+        assertEquals(car.getCurrentSpeed(), car.getX());
+        car.turnRight();
+        car.move();
+        assertEquals(car.getCurrentSpeed(), car.getY());
+        car.turnRight();
+        car.move();
+        assertEquals(car.getCurrentSpeed(), car.getX());
+    }
+
+    @Test
+    public void gas() {
+        car.gas(1);
+        assertEquals(1.25, car.getCurrentSpeed());
+        car.gas(1);
+        assertEquals(2.5, car.getCurrentSpeed());
+        try {
+            car.gas(1.5);
+            assertEquals(1, 2);
+        } catch (IllegalArgumentException e) {
+            assertEquals(1, 1);
+
+        }
+
+    }
+
+    @Test
+    void brake() {
+        car.gas(1);
+        car.brake(1);
+        assertEquals(0, car.getCurrentSpeed());
+        try {
+            car.gas(0.5);
+            car.brake(1.5);
+            assertEquals(1,2);
+        } catch (IllegalArgumentException e){
+            assertEquals(2,2); // detta är bs men ska finnas för att det ska fungera så tänk inte för mycket på siffrorna
+            //Det går även att lägga till så att den inte får minska mer än den gasat men orkar ej lol.
+        }
+
+
+    }
+}
