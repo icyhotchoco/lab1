d27b621e09c759095429d3c45b0c5c0f6b45449f
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-01-29 14:12:07 +0100

    Update CarTest.java

--- CarTest.java
+++ CarTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CarTest {
     private Car car = new Saab95();
 
+
     @Test
     void turnLeft() {
         car.turnLeft();
@@ -43,8 +44,10 @@ class CarTest {
             assertEquals(1, 2);
         } catch (IllegalArgumentException e) {
             assertEquals(1, 1);
-
         }
+        double firstSpeed = car.getCurrentSpeed();
+        car.gas(0.5);
+        assertTrue(firstSpeed < car.getCurrentSpeed());
 
     }
 
@@ -61,7 +64,9 @@ class CarTest {
             assertEquals(2,2); // detta är bs men ska finnas för att det ska fungera så tänk inte för mycket på siffrorna
             //Det går även att lägga till så att den inte får minska mer än den gasat men orkar ej lol.
         }
-
+        double firstSpeed = car.getCurrentSpeed();
+        car.brake(0.5);
+        assertTrue(firstSpeed > car.getCurrentSpeed());
 
     }
 }
