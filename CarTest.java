73e2c7d4ee198c5b01cd04b93c6c447dbac31459
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-01-30 16:06:48 +0100

    Update CarTest.java

--- CarTest.java
+++ CarTest.java
@@ -3,70 +3,111 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    private Car car = new Saab95();
-
+    private Car carS = new Saab95();
+    private Car carV = new Volvo240();
 
     @Test
     void turnLeft() {
-        car.turnLeft();
-        assertEquals(270, car.getDirectionDeg());
+        carS.turnLeft();
+        assertEquals(270, carS.getDirectionDeg());
+        carV.turnLeft();
+        assertEquals(270, carV.getDirectionDeg());
     }
 
     @Test
     void turnRight() {
-        car.turnRight();
-        assertEquals(90, car.getDirectionDeg());
+        carS.turnRight();
+        assertEquals(90, carS.getDirectionDeg());
+        carV.turnRight();
+        assertEquals(90, carV.getDirectionDeg());
     }
 
     @Test
     void move() {
-        car.move();
-        assertEquals(car.getCurrentSpeed(), car.getY());
-        car.turnRight();
-        car.move();
-        assertEquals(car.getCurrentSpeed(), car.getX());
-        car.turnRight();
-        car.move();
-        assertEquals(car.getCurrentSpeed(), car.getY());
-        car.turnRight();
-        car.move();
-        assertEquals(car.getCurrentSpeed(), car.getX());
+        carS.move();
+        assertEquals(carS.getCurrentSpeed(), carS.getY());
+        carS.turnRight();
+        carS.move();
+        assertEquals(carS.getCurrentSpeed(), carS.getX());
+        carS.turnRight();
+        carS.move();
+        assertEquals(carS.getCurrentSpeed(), carS.getY());
+        carS.turnRight();
+        carS.move();
+        assertEquals(carS.getCurrentSpeed(), carS.getX());
+
+        carV.move();
+        assertEquals(carV.getCurrentSpeed(), carV.getY());
+        carV.turnRight();
+        carV.move();
+        assertEquals(carV.getCurrentSpeed(), carV.getX());
+        carV.turnRight();
+        carV.move();
+        assertEquals(carV.getCurrentSpeed(), carV.getY());
+        carV.turnRight();
+        carV.move();
+        assertEquals(carV.getCurrentSpeed(), carV.getX());
     }
 
     @Test
     public void gas() {
-        car.gas(1);
-        assertEquals(1.25, car.getCurrentSpeed());
-        car.gas(1);
-        assertEquals(2.5, car.getCurrentSpeed());
+        carS.gas(1);
+        assertEquals(1.25, carS.getCurrentSpeed());
+        carS.gas(1);
+        assertEquals(2.5, carS.getCurrentSpeed());
         try {
-            car.gas(1.5);
+            carS.gas(1.5);
             assertEquals(1, 2);
         } catch (IllegalArgumentException e) {
             assertEquals(1, 1);
         }
-        double firstSpeed = car.getCurrentSpeed();
-        car.gas(0.5);
-        assertTrue(firstSpeed < car.getCurrentSpeed());
+        double firstSpeed = carS.getCurrentSpeed();
+        carS.gas(0.5);
+        assertTrue(firstSpeed < carS.getCurrentSpeed());
 
+        carV.gas(1);
+        assertEquals(1.25, carV.getCurrentSpeed());
+        carV.gas(1);
+        assertEquals(2.5, carV.getCurrentSpeed());
+        try {
+            carV.gas(1.5);
+            assertEquals(1, 2);
+        } catch (IllegalArgumentException e) {
+            assertEquals(1, 1);
+        }
+        double firstSpeedV = carV.getCurrentSpeed();
+        carV.gas(0.5);
+        assertTrue(firstSpeedV < carV.getCurrentSpeed());
     }
 
     @Test
     void brake() {
-        car.gas(1);
-        car.brake(1);
-        assertEquals(0, car.getCurrentSpeed());
+        carS.gas(1);
+        carS.brake(1);
+        assertEquals(0, carS.getCurrentSpeed());
         try {
-            car.gas(0.5);
-            car.brake(1.5);
+            carS.gas(0.5);
+            carS.brake(1.5);
             assertEquals(1,2);
         } catch (IllegalArgumentException e){
-            assertEquals(2,2); // detta är bs men ska finnas för att det ska fungera så tänk inte för mycket på siffrorna
-            //Det går även att lägga till så att den inte får minska mer än den gasat men orkar ej lol.
+            assertEquals(2,2);
         }
-        double firstSpeed = car.getCurrentSpeed();
-        car.brake(0.5);
-        assertTrue(firstSpeed > car.getCurrentSpeed());
+        double firstSpeed = carS.getCurrentSpeed();
+        carS.brake(0.5);
+        assertTrue(firstSpeed > carS.getCurrentSpeed());
 
+        carV.gas(1);
+        carV.brake(1);
+        assertEquals(0, carS.getCurrentSpeed());
+        try {
+            carV.gas(0.5);
+            carV.brake(1.5);
+            assertEquals(1,2);
+        } catch (IllegalArgumentException e){
+            assertEquals(2,2);
+        }
+        double firstSpeedV = carV.getCurrentSpeed();
+        carV.brake(0.5);
+        assertTrue(firstSpeedV > carV.getCurrentSpeed());
     }
 }
