cb93ce5fad45b7f7aad4210cf416b52dac2673b5
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-06 11:22:53 +0100

    Update TransportCar.java

--- TransportCar.java
+++ TransportCar.java
@@ -1,47 +1,26 @@
 import java.awt.*;
 import java.util.ArrayList;
 
-public class TransportCar extends Car {
+public class TransportCar extends Truck{
     private ArrayList<Car> load;
-    public boolean isUp = false;
-    public TransportCar () {
-        super(2, 250, 0, Color.pink, "CarTransport", 15.0);
-        ArrayList<Car> load = new ArrayList<Car>();
-
+    public TransportCar() {
+        super(2,250,0, Color.PINK,"Car Transport", 15.0, new Ramp(1));
+        ArrayList<Car> load = new ArrayList<>();
     }
-
-    @Override
-    public void gas(double amount) {
-        if (amount > 1 || amount < 0 || isUp == true)
-            throw new IllegalArgumentException();
-        incrementSpeed(amount);
-
-    }
-
-    @Override
-    public double speedFactor() {
-        return enginePower * 0.01;
-    }
-
-    public void raiseRamp() {
-        if (currentSpeed > 0)
-            throw new IllegalArgumentException();
-        isUp = true;
-    }
-    public void lowerRamp() {
-        isUp = false;
-    }
-
-public void addCar (Car car) {
-        if (load.size() > 10 || this.weight < 5.0)
-            throw new IllegalArgumentException();
+    public void addCar(Car car) {
+        if (load.size() > 10 || weight > 5.0 || this.isUndrivable() || this.getDistance(car) > 5) {
+            throw new IllegalStateException();
+        }
         load.add(car);
-        
-}
-
-public void removeCar (Car car) {
-        if (load.size() == 0)
-            throw new IllegalArgumentException();
-        load.remove(car);
-}
+        car.setY(this.getY());
+        car.setX(this.getX());
+    }
+    public void removeCar() {
+        Car car = load.getLast();
+        if (this.isUndrivable()) {
+            throw new IllegalStateException();
+        }
+        load.remove(load.getLast());
+        car.setY(this.getY() - 1);
+    }
 }
