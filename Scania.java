ced3f4c003cf895d6f7a7c944d45e5d932d6a1ce
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-06 11:22:32 +0100

    Update Scania.java

--- Scania.java
+++ Scania.java
@@ -1,34 +1,7 @@
 import java.awt.*;
 
-public class Scania extends Car {
-    int angle = 0;
+public class Scania extends Truck {
     public Scania() {
-        super(2, 200, 0, Color.YELLOW, "Scania");
-    }
-
-    @Override
-    public double speedFactor() {
-        return enginePower * 0.01;
-    }
-
-    @Override
-    public void gas(double amount) {
-        if (amount > 1 || amount < 0 || angle > 0) {
-            throw new IllegalArgumentException("Value needs to be between 0 and 1");
-        }
-        incrementSpeed(amount);
-    }
-    public void platformRaise() {
-        if (currentSpeed > 0) {
-            throw new IllegalArgumentException();
-        }
-        angle = Math.min(angle + 10,70);
-    }
-    public void platformLower() {
-        if (currentSpeed > 0) {
-            throw new IllegalArgumentException();
-        }
-        angle = Math.max(angle - 10,0);
+        super(2, 200, 0, Color.YELLOW, "Scania", 16.0, new Ramp(70));
     }
 }
-
