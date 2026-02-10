266cdf76dcc26d1ba565131e85f4671c9ed115b9
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-10 11:40:05 +0100

    Update Garage.java

--- Garage.java
+++ Garage.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 
-public class Garage <A extends Car> {
+public class Garage <A extends Car>{
     ArrayList<A> garage;
-
-    public Garage() {
-        ArrayList<A> garage = new ArrayList<A>();
-
+    int max;
+    public Garage(int max) {
+        this.garage = new ArrayList<A>();
+        this.max = max;
     }
-    public void addCar(A car) {
+    public void addCar (A car) {
+        if (garage.size() >= max)
+            throw new IllegalStateException("The garage is full");
         garage.add(car);
-
     }
-
+    public A removeCar(A car) {
+        garage.remove(car); //removes the object instead of the index
+        return car;
+    }
 }
-
