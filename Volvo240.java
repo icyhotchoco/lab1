ebd3f0c24a954717022e6cc7fb43f920318d2ba5
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-26 13:33:50 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -2,9 +2,9 @@ import java.awt.*;
 
 public class Volvo240 extends Car {
     private final static double trimFactor = 1.25;
+
     public Volvo240() {
-        super(4, 100,0, Color.black,"Volvo240");
-        stopEngine();
+        super(4, 100,0, Color.black,"Volvo240", 1.4);
     }
     @Override
     public double speedFactor() {
