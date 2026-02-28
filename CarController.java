192a37836becff83f8ccb5806b5e6dbe074037b8
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-28 16:50:51 +0100

    Update CarController.java

--- CarController.java
+++ CarController.java
@@ -11,119 +11,44 @@ import java.util.Arrays;
  */
 
 public class CarController {
-    // member fields:
+    //måste göra cars accessible, arraylistan är nu i carmodel
+    CarView view;
+    CarModel model;
 
-    // The delay (ms) corresponds to 20 updates a sec (hz)
     private final int delay = 50;
-    // The timer is started with a listener (see below) that executes the statements
-    // each step between delays.
-    private Timer timer = new Timer(delay, new TimerListener());
-    // The frame that represents this instance View of the MVC pattern
-    CarView frame;
-    // A list of cars, modify if needed
-    ArrayList<Car> cars = new ArrayList<>();
-    Garage <Volvo240> garage = new Garage<>(10);
-    //methods:
+    protected javax.swing.Timer timer = new Timer(delay, new CarController.TimerListener());
 
-    public static void main(String[] args) {
-        // Instance of this class
-        CarController cc = new CarController();
-        // Add a volvo to list of cars
-        cc.cars.add(new Volvo240());
-        cc.cars.add(new Saab95());
-        cc.cars.add(new Scania());
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            for (int i = cars.size() - 1 ; i >= 0 ; i--) {
+                int x = (int) Math.round(cars.get(i).getX());
+                int y = (int) Math.round(cars.get(i).getY());
+                if (view.overlap(cars.get(i))) {
+                    cars.get(i).turnRight();
+                    cars.get(i).turnRight();
+                }
+                if (cars.get(i) instanceof Volvo240 && model.entersGarage(cars.get(i))) {
+                    garage.addCar((Volvo240) cars.get(i));
+                    x = 1000;
+                    CarViewData carViewData = new CarViewData(x, y, cars.get(i).getModelName());
+                    model.moveit(cars.get(i).hashCode(), carViewData);
+                    cars.remove(cars.get(i));
+                    return;
+                }
 
-        // Start a new view and send a reference of self
-        cc.frame = new CarView("CarSim 1.0", cc);
+                cars.get(i).move();
 
-        // Start the timer
-        cc.timer.start();
+                CarViewData carViewData = new CarViewData(x, y, cars.get(i).getModelName());
+                model.moveit(cars.get(i).hashCode(), carViewData);
+                view.drawPanel.repaint();
+            }
+        }
     }
+
+    // A list of cars, modify if needed
 
     /* Each step the TimerListener moves all the cars in the list and tells the
     * view to update its images. Change this method to your needs.
     * */
-    private class TimerListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            for (int i = cars.size() - 1 ; i >= 0 ; i--) {
-                if (overlap(cars.get(i))) {
-                    cars.get(i).turnRight();
-                    cars.get(i).turnRight();
-                }
-                if (cars.get(i) instanceof Volvo240 && entersGarage(cars.get(i))) {
-                    frame.drawPanel.isInGarage = true;
-                    garage.addCar((Volvo240) cars.get(i));
-                    cars.remove(cars.get(i));
-                }
-                cars.get(i).move();
-                int x = (int) Math.round(cars.get(i).getX());
-                int y = (int) Math.round(cars.get(i).getY());
-                if (cars.get(i) instanceof Volvo240) { frame.drawPanel.volvomoveit(x,y); }
-                if (cars.get(i) instanceof Saab95) { frame.drawPanel.saabmoveit(x,y); }
-                if (cars.get(i) instanceof Scania) { frame.drawPanel.scaniamoveit(x,y); }
-                // repaint() calls the paintComponent method of the panel
-                frame.drawPanel.repaint();
-            }
-        }
-    }
-    public boolean overlap(Car car) {
-        double carSize = (car.getX()) + (frame.drawPanel.volvoImage.getWidth());
-        return (carSize > frame.drawPanel.getWidth() || car.getX() < 0);
-    }
-    public boolean entersGarage(Car car) {
-        double carSize = car.getX() + frame.drawPanel.volvoImage.getWidth();
-        return (carSize >= frame.drawPanel.carWorkshopPoint.getX());
-    }
-    // Calls the gas method for each car once
-    void gas(int amount) {
-        double gas = ((double) amount) / 100;
-        for (Car car : cars) {
-            car.gas(gas);
-        }
-    }
 
-    void brake(int amount) {
-        double brake = ((double) amount) / 100;
-        for (Car car : cars) {
-            car.brake(brake);
-        }
-    }
-    void turboOn() {
-        for (Car car : cars) {
-            if (car instanceof Saab95) {
-                ((Saab95) car).setTurboOn(); //funkar men saab går bara snabbare om man gasar först, åker konstigt
-            }
-        }
-    }
-    void turboOff() {
-        for (Car car : cars) {
-            if (car instanceof Saab95) {
-                ((Saab95) car).setTurboOff(); //samma
-            }
-        }
-    }
-    void raisePlatform() {
-        for (Car car : cars) {
-            if (car instanceof Truck) {
-                ((Truck) car).raisePlatform();
-            }
-        }
-    }
-    void lowerPlatform() {
-        for (Car car : cars) {
-            if (car instanceof Truck) {
-                ((Truck) car).lowerPlatform();
-            }
-        }
-    }
-    void startCars() {
-        for (Car car : cars) {
-            car.startEngine();
-        }
-    }
-    void stopCars() {
-        for (Car car : cars) {
-            car.stopEngine();
-        }
-    }
 }
