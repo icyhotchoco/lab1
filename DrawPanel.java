c80cd64b7c08e0a696ed37950789ab4256a7b31e
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-28 16:51:14 +0100

    Update DrawPanel.java

--- DrawPanel.java
+++ DrawPanel.java
@@ -4,40 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.util.HashMap;
 
 // This panel represents the animated part of the view with the car images.
 
 public class DrawPanel extends JPanel{
 
-    // Just a single image, TODO: Generalize
-    BufferedImage volvoImage;
-    BufferedImage saabImage;
-    BufferedImage scaniaImage;
-    BufferedImage background;
-    // To keep track of a single car's position
-    Point volvoPoint = new Point();
-    boolean isInGarage = false;
-    Point saabPoint = new Point();
-    Point scaniaPoint = new Point();
-
-    BufferedImage carWorkshopImage;
-    Point carWorkshopPoint = new Point(300,0);
-
-    // TODO: Make this general for all cars
-
-    void volvomoveit(int x, int y) { // behöver generaliseras bättre
-        volvoPoint.x = x;
-        volvoPoint.y = y;
-    }
-    void saabmoveit(int x, int y) {
-        saabPoint.x = x;
-        saabPoint.y = y;
-    }
-    void scaniamoveit(int x, int y) {
-        scaniaPoint.x = x;
-        scaniaPoint.y = y;
-
-    }
+    private CarModel model;
+    private BufferedImage background;
+    private BufferedImage carWorkshopImage;
+    private HashMap<String, BufferedImage> carImages = new HashMap<>();
 
     // Initializes the panel and reads the images
     public DrawPanel(int x, int y) {
@@ -46,36 +22,28 @@ public class DrawPanel extends JPanel{
         this.setBackground(Color.lightGray);
         // Print an error message in case file is not found with a try/catch block
         try {
-            // You can remove the "pics" part if running outside of IntelliJ and
-            // everything is in the same main folder.
-            //carImage = ImageIO.read(new File("pictures/volvo.png"));
-
-            // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
-            // if you are starting in IntelliJ.
-            scaniaImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/Scania.png"));
-            saabImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/Saab95.png"));
-            volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/Volvo240.png"));
+            BufferedImage scaniaImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/Scania.png"));
+            BufferedImage saabImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/Saab95.png"));
+            BufferedImage volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/Volvo240.png"));
             carWorkshopImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/VolvoBrand.png"));
             background = ImageIO.read(DrawPanel.class.getResourceAsStream("pictures/14104_113697.jpg"));
-
+            carImages.put("Volvo240", volvoImage);
+            carImages.put("Saab95", saabImage);
+            carImages.put("Scania", scaniaImage);
         } catch (IOException ex)
         {
             ex.printStackTrace();
         }
     }
-
     // This method is called each time the panel updates/refreshes/repaints itself
     // TODO: Change to suit your needs.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(background,0,0,null);
-        if (!isInGarage) {
-            g.drawImage(volvoImage, volvoPoint.x, volvoPoint.y, null);
+        g.drawImage(background, 0, 0, null);
+        for (CarViewData car : model.position.values()) {
+            g.drawImage(carImages.get(car.getName()), car.getX(), car.getY(), null);
         }
-
-        g.drawImage(saabImage, saabPoint.x, (saabPoint.y + 100), null);
-        g.drawImage(scaniaImage, scaniaPoint.x, (scaniaPoint.y + 200), null);
-        g.drawImage(carWorkshopImage, carWorkshopPoint.x, carWorkshopPoint.y, null);
+        g.drawImage(carWorkshopImage, model.carWorkshopPoint.x, model.carWorkshopPoint.y, null);
     }
 }
