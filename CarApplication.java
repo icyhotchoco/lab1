d3473711d2f3ae4f1f98344b218aeaf74de2cb79
icyhotchoco <159943394+icyhotchoco@users.noreply.github.com>
2026-02-28 19:24:03 +0100

    Update CarApplication.java

--- CarApplication.java
+++ CarApplication.java
@@ -3,19 +3,20 @@ public class CarApplication {
     public static void main(String[] args) {
         // Instance of this class
         CarModel model = new CarModel();
-        CarController cc = new CarController();
+        CarView view = new CarView("CarSim 1.0", model);
+        CarController cc = new CarController(view,model);
         Volvo240 volvo240 = new Volvo240();
         Saab95 saab95 = new Saab95();
         Scania scania = new Scania();
         saab95.setY(100.0);
         scania.setY(200.0);
         // Add a volvo to list of cars
-        model.cars.add(volvo240);
-        model.cars.add(saab95);
-        model.cars.add(scania);
+        model.addCar(volvo240);
+        model.addCar(saab95);
+        model.addCar(scania);
 
         // Start a new view and send a reference of self
-        cc.view = new CarView("CarSim 1.0", model);
+        //cc.view = new CarView("CarSim 1.0", model);
 
         // Start the timer
         cc.timer.start();
